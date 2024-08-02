05e686d9930dba2ca705dbee2ba92dccf9a87f1c
NUCLEAR-WAR <63456208+NUCLEAR-WAR@users.noreply.github.com>
2024-08-03 01:51:01 +0200

    Update pre-install.sh
    add more dependencies

--- debian/pre-install.sh
+++ debian/pre-install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 #upgrade the packages
+curl -sSL https://packages.sury.org/php/README.txt | bash -x
 apt-get update && apt-get upgrade -y
 
 #install packages
-apt-get install -y git lsb-release iptables
+apt-get install -y git lsb-release iptables ca-certificates software-properties-common apt-transport-https php8.1 php8.1-fpm libapache2-mod-fcgid php8.1-fpm php8.1-cli
 
 #get the install script
 cd /usr/src && git clone https://github.com/NUCLEAR-WAR/fusionpbx-custom.git
