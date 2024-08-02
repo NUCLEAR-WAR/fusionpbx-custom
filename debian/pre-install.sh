e10059052daeba22a688796aff53c34d85bc5b77
NUCLEAR-WAR <63456208+NUCLEAR-WAR@users.noreply.github.com>
2024-08-03 00:13:51 +0200

    Update pre-install.sh

--- debian/pre-install.sh
+++ debian/pre-install.sh
@@ -11,3 +11,5 @@ cd /usr/src && git clone https://github.com/NUCLEAR-WAR/fusionpbx-custom.git
 
 #change the working directory
 cd /usr/src/fusionpbx-install.sh/debian
+chmod install.sh 755
+./install.sh
